6b852e1a9d384ff182c4b71ee32d1988f9ad8697
Shivanand Chikkalli <sychikkalli@gmail.com>
2021-04-02 23:48:28 +0530

    extract db data to ms-excel - initial commit

---
+++ ExtractDBDataToExcel/ExtractDBDataToExcel/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ExtractDBDataToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serverUrl_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.password_textBox = new System.Windows.Forms.TextBox();
+            this.filepath_textBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.runScript_button = new System.Windows.Forms.Button();
+            this.browseScript_button = new System.Windows.Forms.Button();
+            this.reset_button = new System.Windows.Forms.Button();
+            this.remember_checkBox = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // serverUrl_textBox
+            // 
+            this.serverUrl_textBox.Location = new System.Drawing.Point(100, 33);
+            this.serverUrl_textBox.Name = "serverUrl_textBox";
+            this.serverUrl_textBox.Size = new System.Drawing.Size(227, 20);
+            this.serverUrl_textBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "User Name";
+            // 
+            // username_textBox
+            // 
+            this.username_textBox.Location = new System.Drawing.Point(436, 33);
+            this.username_textBox.Name = "username_textBox";
+            this.username_textBox.Size = new System.Drawing.Size(166, 20);
+            this.username_textBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(646, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // password_textBox
+            // 
+            this.password_textBox.Location = new System.Drawing.Point(705, 32);
+            this.password_textBox.Name = "password_textBox";
+            this.password_textBox.PasswordChar = '*';
+            this.password_textBox.Size = new System.Drawing.Size(171, 20);
+            this.password_textBox.TabIndex = 4;
+            // 
+            // filepath_textBox
+            // 
+            this.filepath_textBox.Enabled = false;
+            this.filepath_textBox.Location = new System.Drawing.Point(1190, 33);
+            this.filepath_textBox.Name = "filepath_textBox";
+            this.filepath_textBox.Size = new System.Drawing.Size(216, 20);
+            this.filepath_textBox.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // runScript_button
+            // 
+            this.runScript_button.Location = new System.Drawing.Point(1480, 31);
+            this.runScript_button.Name = "runScript_button";
+            this.runScript_button.Size = new System.Drawing.Size(108, 23);
+            this.runScript_button.TabIndex = 10;
+            this.runScript_button.Text = "Run Script";
+            this.runScript_button.UseVisualStyleBackColor = true;
+            this.runScript_button.Click += new System.EventHandler(this.runScript_button_Click);
+            // 
+            // browseScript_button
+            // 
+            this.browseScript_button.Location = new System.Drawing.Point(1059, 31);
+            this.browseScript_button.Name = "browseScript_button";
+            this.browseScript_button.Size = new System.Drawing.Size(125, 23);
+            this.browseScript_button.TabIndex = 8;
+            this.browseScript_button.Text = "Browse Script File";
+            this.browseScript_button.UseVisualStyleBackColor = true;
+            this.browseScript_button.Click += new System.EventHandler(this.browseScript_button_Click);
+            // 
+            // reset_button
+            // 
+            this.reset_button.Location = new System.Drawing.Point(1619, 31);
+            this.reset_button.Name = "reset_button";
+            this.reset_button.Size = new System.Drawing.Size(108, 23);
+            this.reset_button.TabIndex = 12;
+            this.reset_button.Text = "Reset";
+            this.reset_button.UseVisualStyleBackColor = true;
+            this.reset_button.Click += new System.EventHandler(this.reset_button_Click);
+            // 
+            // remember_checkBox
+            // 
+            this.remember_checkBox.AutoSize = true;
+            this.remember_checkBox.Location = new System.Drawing.Point(921, 35);
+            this.remember_checkBox.Name = "remember_checkBox";
+            this.remember_checkBox.Size = new System.Drawing.Size(77, 17);
+            this.remember_checkBox.TabIndex = 6;
+            this.remember_checkBox.Text = "Remember";
+            this.remember_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(34, 166);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1693, 381);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtering Section";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1774, 764);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.remember_checkBox);
+            this.Controls.Add(this.reset_button);
+            this.Controls.Add(this.browseScript_button);
+            this.Controls.Add(this.runScript_button);
+            this.Controls.Add(this.filepath_textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.password_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.username_textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serverUrl_textBox);
+            this.Name = "Form1";
+            this.Text = "Extract Data To Excel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox serverUrl_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username_textBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox password_textBox;
+        private System.Windows.Forms.TextBox filepath_textBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button runScript_button;
+        private System.Windows.Forms.Button browseScript_button;
+        private System.Windows.Forms.Button reset_button;
+        private System.Windows.Forms.CheckBox remember_checkBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ExtractDBDataToExcel/ExtractDBDataToExcel/Form1.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace ExtractDBDataToExcel
+{
+    public partial class Form1 : Form
+    {
+        private string userScript = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseScript_button_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                filepath_textBox.Text = file;
+                try
+                {
+                    userScript = File.ReadAllText(file);
+                    if(userScript.Length == 0)
+                        MessageBox.Show("Empty File!");
+                }
+                catch (IOException ioex)
+                {
+                    MessageBox.Show(ioex.Message);
+                }
+            }
+        }
+
+        private void runScript_button_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(serverUrl_textBox.Text.Trim()))
+            {
+                MessageBox.Show("Please enter a Server URL");
+                serverUrl_textBox.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(username_textBox.Text.Trim()))
+            {
+                MessageBox.Show("Please enter a User Name");
+                username_textBox.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password_textBox.Text.Trim()))
+            {
+                MessageBox.Show("Please enter a Password");
+                password_textBox.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(filepath_textBox.Text.Trim()))
+            {
+                MessageBox.Show("Please select a script file");
+                password_textBox.Focus();
+                return;
+            }
+            UserInputs userInputs = new UserInputs()
+            {
+                ServerUrl = serverUrl_textBox.Text,
+                UserName = username_textBox.Text,
+                Password = password_textBox.Text
+            };
+            if (remember_checkBox.Checked == true)
+            {
+                saveUserInputs(userInputs);
+            }
+
+            executeScript(userInputs);
+        }
+
+        private void reset_button_Click(object sender, EventArgs e)
+        {
+            serverUrl_textBox.ResetText();
+            username_textBox.ResetText();
+            password_textBox.ResetText();
+            filepath_textBox.ResetText();
+        }
+
+        private void saveUserInputs(UserInputs userInputs)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(UserInputs));
+
+            string serverDetailsFilename = string.Concat(Regex.Replace(serverUrl_textBox.Text, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled) , ".xml");
+
+            using (TextWriter textWriter = new StreamWriter(serverDetailsFilename))
+            {
+                serializer.Serialize(textWriter, userInputs);
+            }
+        }
+
+
+        private void CreateFileOrFolder(string filename)
+        {
+            string folderName = @"ServerFileStructure";   
+                
+            string pathString = System.IO.Path.Combine(folderName, "UserInputs");
+                
+            System.IO.Directory.CreateDirectory(pathString);
+
+            string fileName = filename;
+
+            pathString = System.IO.Path.Combine(pathString, fileName);
+
+            if (!System.IO.File.Exists(pathString))
+            {
+                System.IO.FileStream fs = System.IO.File.Create(pathString);
+            }
+            else
+            {
+                Console.WriteLine("File \"{0}\" already exists.", fileName);
+                return;
+            }
+        }
+
+        private void executeScript(UserInputs userInputs)
+        {
+            try
+            {
+                string str = string.Concat("server=" , userInputs.ServerUrl, ";UID=", userInputs.UserName + "; password=", userInputs.Password);
+
+                string query = userScript;
+
+                SqlConnection con = new SqlConnection(str);
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Open();
+
+                DataSet dataSet = new DataSet();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(dataSet);
+
+                try
+                {
+                    Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+
+                    Workbook xlWorkbook = ExcelApp.Workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet);
+
+                    DataTableCollection collection = dataSet.Tables;
+
+                    for (int i = collection.Count; i > 0; i--)
+                    {
+                        Sheets xlSheets = null;
+                        Worksheet xlWorksheet = null;
+
+                        //Create Excel Sheets
+                        xlSheets = ExcelApp.Sheets;
+
+                        xlWorksheet = (Worksheet)xlSheets.Add(xlSheets[1], Type.Missing, Type.Missing, Type.Missing);
+                        
+                        System.Data.DataTable table = collection[i - 1];
+
+                        xlWorksheet.Name = table.TableName;
+
+                        for (int j = 1; j < table.Columns.Count + 1; j++)
+                        {
+                            ExcelApp.Cells[1, j] = table.Columns[j - 1].ColumnName;
+                        }
+                        // Storing Each row and column value to excel sheet
+                        for (int k = 0; k < table.Rows.Count; k++)
+                        {
+                            for (int l = 0; l < table.Columns.Count; l++)
+                            {
+                                ExcelApp.Cells[k + 2, l + 1] = table.Rows[k].ItemArray[l].ToString();
+                            }
+                        }
+                        ExcelApp.Columns.AutoFit();
+                    }
+
+                    ((Worksheet)ExcelApp.ActiveWorkbook.Sheets[ExcelApp.ActiveWorkbook.Sheets.Count]).Delete();
+
+                    ExcelApp.Visible = true;
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                con.Close();
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+        }
+    }
+
+    public class UserInputs
+    {
+        public string ServerUrl { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
